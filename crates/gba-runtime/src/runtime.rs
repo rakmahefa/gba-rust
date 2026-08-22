2fd24674a02f11f78c08ce66ef620094410175cf
rakmahefa <rakmahefa@gmail.com>
2026-08-22 12:26:22 +0300

    fix(runtime): align integrated MMIO behavior and formatting

--- crates/gba-runtime/src/runtime.rs
+++ crates/gba-runtime/src/runtime.rs
@@ -187,11 +187,18 @@ impl Runtime {
             vram: &mut self.vram,
             oam: &mut self.oam,
         };
-        execute_bios_swi(&mut self.cpu, &mut self.power, &mut self.interrupts, &mut memory, swi)
+        execute_bios_swi(
+            &mut self.cpu,
+            &mut self.power,
+            &mut self.interrupts,
+            &mut memory,
+            swi,
+        )
     }
 
     pub fn bios_swi_number(&mut self, raw: u32, thumb: bool) -> Option<BiosResult> {
-        BiosSwi::from_number(super::bios::swi_number(raw, thumb)).map(|swi| self.bios_swi(swi))
+        let number = super::bios::swi_number(raw, thumb);
+        BiosSwi::from_number(number).map(|swi| self.bios_swi(swi))
     }
 
     pub fn request_interrupt(&mut self, mask: u16) {
@@ -234,33 +241,26 @@ impl Runtime {
     }
 
     pub fn read8(&self, address: u32) -> u8 {
-        if (0x0000_0000..=0x0000_3fff).contains(&address) {
-            0xff
-        } else if (0x0200_0000..=0x0203_ffff).contains(&address) {
-            self.ewram[(address - 0x0200_0000) as usize]
-        } else if (0x0300_0000..=0x0300_7fff).contains(&address) {
-            self.iwram[(address - 0x0300_0000) as usize]
-        } else if (0x0400_0000..=0x0400_03ff).contains(&address) {
-            self.read_mmio8(address)
-        } else if (0x0500_0000..=0x0500_03ff).contains(&address) {
-            self.palette[(address - 0x0500_0000) as usize]
-        } else if (0x0600_0000..=0x0601_7fff).contains(&address) {
-            self.vram[(address - 0x0600_0000) as usize]
-        } else if (0x0700_0000..=0x0700_03ff).contains(&address) {
-            self.oam[(address - 0x0700_0000) as usize]
-        } else if (0x0800_0000..0x0e00_0000).contains(&address) {
-            self.cartridge
+        match address {
+            0x0000_0000..=0x0000_3fff => 0xff,
+            0x0200_0000..=0x0203_ffff => self.ewram[(address - 0x0200_0000) as usize],
+            0x0300_0000..=0x0300_7fff => self.iwram[(address - 0x0300_0000) as usize],
+            0x0400_0000..=0x0400_03ff => self.read_mmio8(address),
+            0x0500_0000..=0x0500_03ff => self.palette[(address - 0x0500_0000) as usize],
+            0x0600_0000..=0x0601_7fff => self.vram[(address - 0x0600_0000) as usize],
+            0x0700_0000..=0x0700_03ff => self.oam[(address - 0x0700_0000) as usize],
+            0x0800_0000..0x0e00_0000 => self
+                .cartridge
                 .as_ref()
                 .and_then(|c| c.rom.get((address - 0x0800_0000) as usize))
                 .copied()
-                .unwrap_or(0xff)
-        } else if (0x0e00_0000..=0x0e00_ffff).contains(&address) {
-            self.cartridge
+                .unwrap_or(0xff),
+            0x0e00_0000..=0x0e00_ffff => self
+                .cartridge
                 .as_ref()
                 .map(|c| c.save.read((address - 0x0e00_0000) as usize))
-                .unwrap_or(0xff)
-        } else {
-            *self.io.get(&address).unwrap_or(&0)
+                .unwrap_or(0xff),
+            _ => *self.io.get(&address).unwrap_or(&0),
         }
     }
 
@@ -268,24 +268,27 @@ impl Runtime {
         match address {
             0x0400_0004 => self.dispstat as u8,
             0x0400_0005 => (self.dispstat >> 8) as u8,
-            0x0400_0130 => self.keyinput as u8,
-            0x0400_0131 => (self.keyinput >> 8) as u8,
-            0x0400_0200 => self.interrupts.ie as u8,
-            0x0400_0201 => (self.interrupts.ie >> 8) as u8,
-            0x0400_0202 => self.interrupts.iflags as u8,
-            0x0400_0203 => (self.interrupts.iflags >> 8) as u8,
-            0x0400_0204 => self.waitcnt as u8,
-            0x0400_0205 => (self.waitcnt >> 8) as u8,
-            0x0400_0208 => u8::from(self.interrupts.ime),
+            KEYINPUT => self.keyinput as u8,
+            KEYINPUT + 1 => (self.keyinput >> 8) as u8,
+            IE => self.interrupts.ie as u8,
+            IE + 1 => (self.interrupts.ie >> 8) as u8,
+            IF => self.interrupts.iflags as u8,
+            IF + 1 => (self.interrupts.iflags >> 8) as u8,
+            WAITCNT => self.waitcnt as u8,
+            WAITCNT + 1 => (self.waitcnt >> 8) as u8,
+            IME => u8::from(self.interrupts.ime),
             0x0400_0300 => self.postflg,
-            0x0400_0301 => 0,
+            HALTCNT => 0,
             _ => *self.io.get(&address).unwrap_or(&0),
         }
     }
 
     pub fn read16(&self, address: u32) -> u16 {
         if (0x0400_0000..=0x0400_03ff).contains(&address) {
-            u16::from_le_bytes([self.read_mmio8(address), self.read_mmio8(address.wrapping_add(1))])
+            u16::from_le_bytes([
+                self.read_mmio8(address),
+                self.read_mmio8(address.wrapping_add(1)),
+            ])
         } else {
             u16::from_le_bytes([self.read8(address), self.read8(address.wrapping_add(1))])
         }
@@ -303,26 +306,33 @@ impl Runtime {
     }
 
     pub fn write8(&mut self, address: u32, value: u8) {
-        if (0x0200_0000..=0x0203_ffff).contains(&address) {
-            self.ewram[(address - 0x0200_0000) as usize] = value;
-        } else if (0x0300_0000..=0x0300_7fff).contains(&address) {
-            self.iwram[(address - 0x0300_0000) as usize] = value;
-        } else if (0x0400_0000..=0x0400_03ff).contains(&address) {
-            self.write_mmio8(address, value);
-        } else if (0x0500_0000..=0x0500_03ff).contains(&address) {
-            self.palette[(address - 0x0500_0000) as usize] = value;
-        } else if (0x0600_0000..=0x0601_7fff).contains(&address) {
-            self.vram[(address - 0x0600_0000) as usize] = value;
-        } else if (0x0700_0000..=0x0700_03ff).contains(&address) {
-            self.oam[(address - 0x0700_0000) as usize] = value;
-        } else if (0x0e00_0000..=0x0e00_ffff).contains(&address) {
-            if let Some(cartridge) = self.cartridge.as_mut() {
-                cartridge
-                    .save
-                    .write((address - 0x0e00_0000) as usize, value);
+        match address {
+            0x0200_0000..=0x0203_ffff => {
+                self.ewram[(address - 0x0200_0000) as usize] = value;
             }
-        } else {
-            self.io.insert(address, value);
+            0x0300_0000..=0x0300_7fff => {
+                self.iwram[(address - 0x0300_0000) as usize] = value;
+            }
+            0x0400_0000..=0x0400_03ff => self.write_mmio8(address, value),
+            0x0500_0000..=0x0500_03ff => {
+                self.palette[(address - 0x0500_0000) as usize] = value;
+            }
+            0x0600_0000..=0x0601_7fff => {
+                self.vram[(address - 0x0600_0000) as usize] = value;
+            }
+            0x0700_0000..=0x0700_03ff => {
+                self.oam[(address - 0x0700_0000) as usize] = value;
+            }
+            0x0e00_0000..=0x0e00_ffff => {
+                if let Some(cartridge) = self.cartridge.as_mut() {
+                    cartridge
+                        .save
+                        .write((address - 0x0e00_0000) as usize, value);
+                }
+            }
+            _ => {
+                self.io.insert(address, value);
+            }
         }
     }
 
@@ -330,14 +340,14 @@ impl Runtime {
         match address {
             0x0400_0004 => self.dispstat = (self.dispstat & 0xff00) | value as u16,
             0x0400_0005 => self.dispstat = (self.dispstat & 0x00ff) | ((value as u16) << 8),
-            0x0400_0130 | 0x0400_0131 => {}
-            0x0400_0200 => self.interrupts.ie = (self.interrupts.ie & 0xff00) | value as u16,
-            0x0400_0201 => self.interrupts.ie = (self.interrupts.ie & 0x00ff) | ((value as u16) << 8),
-            0x0400_0202 => self.interrupts.acknowledge(value as u16),
-            0x0400_0203 => self.interrupts.acknowledge((value as u16) << 8),
-            0x0400_0204 => self.waitcnt = (self.waitcnt & 0xff00) | value as u16,
-            0x0400_0205 => self.waitcnt = (self.waitcnt & 0x00ff) | ((value as u16) << 8),
-            0x0400_0208 => {
+            KEYINPUT | (KEYINPUT + 1) => {}
+            IE => self.interrupts.ie = (self.interrupts.ie & 0xff00) | value as u16,
+            IE + 1 => self.interrupts.ie = (self.interrupts.ie & 0x00ff) | ((value as u16) << 8),
+            IF => self.interrupts.acknowledge(value as u16),
+            IF + 1 => self.interrupts.acknowledge((value as u16) << 8),
+            WAITCNT => self.waitcnt = (self.waitcnt & 0xff00) | value as u16,
+            WAITCNT + 1 => self.waitcnt = (self.waitcnt & 0x00ff) | ((value as u16) << 8),
+            IME => {
                 self.interrupts.ime = value & 1 != 0;
                 if self.interrupts.ime {
                     self.service_interrupts();
@@ -351,9 +361,7 @@ impl Runtime {
                     self.power = PowerState::Halted;
                 }
             }
-            _ => {
-                self.io.insert(address, value);
-            }
+            _ => self.io.insert(address, value),
         }
     }
 
@@ -581,7 +589,10 @@ mod tests {
     #[test]
     fn exchange_target_for_dispatch_updates_thumb_and_pc() {
         let mut runtime = Runtime::new();
-        assert_eq!(runtime.exchange_target_for_dispatch(0x0800_0101), (0x0800_0100, true));
+        assert_eq!(
+            runtime.exchange_target_for_dispatch(0x0800_0101),
+            (0x0800_0100, true)
+        );
         assert!(runtime.cpu.thumb);
         assert_eq!(runtime.read_reg(REG_PC), 0x0800_0100);
     }
@@ -591,7 +602,7 @@ mod tests {
         let mut runtime = Runtime::new();
         runtime.write16(IE, IRQ_VBLANK);
         runtime.write16(IME, 1);
-        runtime.write16(IF, IRQ_VBLANK);
+        runtime.request_interrupt(IRQ_VBLANK);
         assert_eq!(runtime.read16(IE), IRQ_VBLANK);
         assert_eq!(runtime.read16(IF), IRQ_VBLANK);
         assert_eq!(runtime.mode(), CpuMode::Irq);
