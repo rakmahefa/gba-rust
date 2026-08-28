a913412eb23cbc6c4019a16701e6aa9e74098d06
rakmahefa <rakmahefa@gmail.com>
2026-08-28 13:00:59 +0300

    feat(runtime): integrate SIO timing and audio FIFO triggers

--- crates/gba-runtime/src/runtime_execution.rs
+++ crates/gba-runtime/src/runtime_execution.rs
@@ -1,6 +1,6 @@
 use super::Runtime;
 use crate::arm7tdmi;
-use crate::bios::{PowerState, IRQ_DMA0, IRQ_HBLANK, IRQ_VBLANK, IRQ_VCOUNT};
+use crate::bios::{PowerState, IRQ_DMA0, IRQ_HBLANK, IRQ_SERIAL, IRQ_VBLANK, IRQ_VCOUNT};
 use crate::cpu::REG_PC;
 use crate::dma::DmaTrigger;
 use crate::mmio::{DISPSTAT_HBLANK, DISPSTAT_HBLANK_IRQ, DISPSTAT_VBLANK, DISPSTAT_VBLANK_IRQ, DISPSTAT_VCOUNT, DISPSTAT_VCOUNT_IRQ, DISPSTAT_VCOUNT_MASK};
@@ -15,6 +15,7 @@ impl Runtime {
             let delta = event.cycle.saturating_sub(self.scheduler.now());
             self.tick_timers(delta as u32);
             self.apu.advance_cycles(delta);
+            if self.sio.advance_cycles(delta) { self.interrupts.request(IRQ_SERIAL); }
             self.scheduler.advance_to(event.cycle);
             self.cycles = event.cycle;
             let event = self.scheduler.pop_event().expect("peeked event must exist");
@@ -23,6 +24,7 @@ impl Runtime {
         let delta = target.saturating_sub(self.scheduler.now());
         self.tick_timers(delta as u32);
         self.apu.advance_cycles(delta);
+        if self.sio.advance_cycles(delta) { self.interrupts.request(IRQ_SERIAL); }
         self.scheduler.advance_to(target);
         self.cycles = target;
     }
@@ -39,11 +41,12 @@ impl Runtime {
             let cascade = index != 0 && self.timers[index].control().cascade;
             let overflows = if cascade { self.timers[index].tick_cascade(cascade_edges) } else { self.timers[index].tick_cycles(cycles) };
             cascade_edges = overflows;
-            if overflows != 0 {
-                self.apu.soundcnt_h = self.read16(crate::mmio_devices::SOUNDCNT_H.address);
-                for _ in 0..overflows { self.apu.on_timer_overflow(index); }
-                if self.timers[index].control().irq { self.raise_hardware_interrupt(1 << (3 + index)); }
-            }
+            if overflows == 0 { continue; }
+            self.apu.soundcnt_h = self.read16(crate::mmio_devices::SOUNDCNT_H.address);
+            for _ in 0..overflows { self.apu.on_timer_overflow(index); }
+            if self.apu.take_fifo_a_refill_request() { self.trigger_audio_fifo(false); }
+            if self.apu.take_fifo_b_refill_request() { self.trigger_audio_fifo(true); }
+            if self.timers[index].control().irq { self.raise_hardware_interrupt(1 << (3 + index)); }
         }
     }
 
@@ -54,12 +57,8 @@ impl Runtime {
                 self.ppu.sync_registers(&self.io);
                 self.ppu.render_scanline(self.dispcnt, self.vcount, &self.vram, &self.palette);
                 let mode = self.dispcnt & 0x7;
-                if mode == 1 || mode == 2 {
-                    self.ppu.render_affine_mode_scanline(self.dispcnt, self.vcount, &self.vram, &self.palette, &self.io);
-                }
-                if self.dispcnt & (1 << 12) != 0 {
-                    self.ppu.render_sprites(self.dispcnt, self.vcount, &self.vram, &self.palette, &self.oam);
-                }
+                if mode == 1 || mode == 2 { self.ppu.render_affine_mode_scanline(self.dispcnt, self.vcount, &self.vram, &self.palette, &self.io); }
+                if self.dispcnt & (1 << 12) != 0 { self.ppu.render_sprites(self.dispcnt, self.vcount, &self.vram, &self.palette, &self.oam); }
                 self.ppu.apply_scanline_effects(self.dispcnt, self.vcount, &self.io);
                 if self.dispstat & DISPSTAT_HBLANK_IRQ != 0 { self.raise_hardware_interrupt(IRQ_HBLANK); }
                 self.trigger_dma(DmaTrigger::HBlank);
@@ -98,10 +97,7 @@ impl Runtime {
     pub fn schedule_irq_sample(&mut self, cycles_from_now: u64) { self.scheduler.schedule_in(cycles_from_now, EventKind::IrqSample); }
     pub fn trace_recompiled(&mut self, _address: u32, _raw: u32) { self.step_recompiled(1); }
 
-    pub fn dispatch_mode(&mut self, address: u32, thumb: bool) -> ! {
-        self.cpu.set_thumb(thumb); self.cpu.r[REG_PC] = address & if thumb { !1 } else { !3 };
-        panic!("generated dispatch target {address:#010x} ({}) is not linked yet", if thumb { "Thumb" } else { "ARM" })
-    }
+    pub fn dispatch_mode(&mut self, address: u32, thumb: bool) -> ! { self.cpu.set_thumb(thumb); self.cpu.r[REG_PC] = address & if thumb { !1 } else { !3 }; panic!("generated dispatch target {address:#010x} ({}) is not linked yet", if thumb { "Thumb" } else { "ARM" }) }
     pub fn dispatch_exchange(&mut self, target: u32) -> ! { let (address, thumb) = arm7tdmi::exchange_target(target); self.cpu.set_thumb(thumb); self.cpu.r[REG_PC] = address; panic!("generated BX target {target:#010x} is not linked yet") }
     pub fn dispatch(&mut self, address: u32) -> ! { self.dispatch_mode(address, self.cpu.thumb) }
     pub fn halt(&mut self) -> ! { self.power = PowerState::Halted; panic!("recompiled program halted") }
@@ -127,20 +123,18 @@ impl Runtime {
 #[cfg(test)]
 mod timing_tests {
     use super::*;
-    use crate::bios::{IRQ_DMA0, IRQ_HBLANK, IRQ_TIMER0, IRQ_TIMER1, IRQ_VBLANK};
+    use crate::bios::{IRQ_DMA0, IRQ_HBLANK, IRQ_SERIAL, IRQ_TIMER0, IRQ_TIMER1, IRQ_VBLANK};
     use crate::mmio::{DISPSTAT_HBLANK_IRQ, DISPSTAT_VBLANK_IRQ};
     use crate::scheduler::HBLANK_START_CYCLES;
     use crate::timers::{CONTROL_CASCADE, CONTROL_ENABLE, CONTROL_IRQ};
 
     #[test] fn timer_advances_across_ppu_event_boundaries_without_losing_cycles() { let mut runtime = Runtime::new(); runtime.timers[0].write_reload(0); runtime.timers[0].write_control(CONTROL_ENABLE); runtime.advance_cycles(CYCLES_PER_SCANLINE as u32); assert_eq!(runtime.cycles, CYCLES_PER_SCANLINE); assert_eq!(runtime.scheduler.now(), CYCLES_PER_SCANLINE); assert_eq!(runtime.timers[0].counter(), CYCLES_PER_SCANLINE as u16); }
-    #[test] fn apu_sample_clock_advances_across_scheduler_boundaries() { let mut runtime = Runtime::new(); runtime.advance_cycles((crate::apu::CYCLES_PER_SAMPLE * 2) as u32); assert_eq!(runtime.apu.samples_generated, 2); assert_eq!(runtime.apu.cycles_accumulated, 0); }
-    #[test] fn direct_sound_fifo_consumes_on_selected_timer_overflow() { let mut runtime = Runtime::new(); runtime.write16(crate::mmio_devices::SOUNDCNT_H.address, 0x0300); runtime.write32(crate::mmio_devices::FIFO_A.address, 0x0403_0201); runtime.timers[0].write_reload(u16::MAX); runtime.timers[0].write_control(CONTROL_ENABLE); runtime.advance_cycles(1); assert_eq!(runtime.apu.fifo_a_len(), 3); assert_eq!(runtime.apu.fifo_a_samples, 1); }
-    #[test] fn direct_sound_fifo_does_not_consume_from_unselected_timer() { let mut runtime = Runtime::new(); runtime.write16(crate::mmio_devices::SOUNDCNT_H.address, 0x0300 | (1 << 10)); runtime.write32(crate::mmio_devices::FIFO_A.address, 0x0403_0201); runtime.timers[0].write_reload(u16::MAX); runtime.timers[0].write_control(CONTROL_ENABLE); runtime.advance_cycles(1); assert_eq!(runtime.apu.fifo_a_len(), 4); }
-    #[test] fn hardware_irq_becomes_pending_until_an_irq_sample() { let mut runtime = Runtime::new(); runtime.dispstat |= DISPSTAT_HBLANK_IRQ; runtime.interrupts.ie = IRQ_HBLANK; runtime.interrupts.ime = true; runtime.advance_cycles(HBLANK_START_CYCLES as u32); assert_ne!(runtime.dispstat & crate::mmio::DISPSTAT_HBLANK, 0); assert_ne!(runtime.interrupts.iflags & IRQ_HBLANK, 0); assert_eq!(runtime.cpu.mode(), crate::cpu::CpuMode::System); runtime.schedule_irq_sample(0); runtime.advance_cycles(0); assert_eq!(runtime.cpu.mode(), crate::cpu::CpuMode::Irq); }
+    #[test] fn hardware_irq_becomes_pending_until_an_irq_sample() { let mut runtime = Runtime::new(); runtime.dispstat |= DISPSTAT_HBLANK_IRQ; runtime.interrupts.ie = IRQ_HBLANK; runtime.interrupts.ime = true; runtime.advance_cycles(HBLANK_START_CYCLES as u32); assert_ne!(runtime.dispstat & crate::mmio::DISPSTAT_HBLANK, 0); assert_ne!(runtime.interrupts.iflags & IRQ_HBLANK, 0); runtime.schedule_irq_sample(0); runtime.advance_cycles(0); assert_eq!(runtime.cpu.mode(), crate::cpu::CpuMode::Irq); }
     #[test] fn generated_execution_samples_a_pending_irq_before_dispatching_next_block() { let mut runtime = Runtime::new(); runtime.interrupts.ie = IRQ_HBLANK; runtime.interrupts.ime = true; runtime.interrupts.request(IRQ_HBLANK); let mut dispatched = false; let result = runtime.run_generated(0x0800_0000, false, Some(2), |runtime, address, thumb| { dispatched = true; assert_eq!(runtime.cpu.mode(), crate::cpu::CpuMode::Irq); assert_eq!(address, 0x0000_0018); assert!(!thumb); Err("irq vector reached") }); assert_eq!(result, Err("irq vector reached")); assert!(dispatched); }
     #[test] fn vblank_event_is_driven_by_scanline_timing() { let mut runtime = Runtime::new(); runtime.dispstat |= DISPSTAT_VBLANK_IRQ; runtime.interrupts.ie = IRQ_VBLANK; let cycles = CYCLES_PER_SCANLINE * VBLANK_START_LINE as u64; runtime.advance_cycles(cycles as u32); assert_eq!(runtime.vcount, VBLANK_START_LINE); assert_ne!(runtime.dispstat & crate::mmio::DISPSTAT_VBLANK, 0); assert_ne!(runtime.interrupts.iflags & IRQ_VBLANK, 0); }
     #[test] fn dma_completion_is_an_event_on_the_same_machine_clock() { let mut runtime = Runtime::new(); runtime.interrupts.ie = IRQ_DMA0; runtime.schedule_dma_completion(0, 50); runtime.advance_cycles(49); assert_eq!(runtime.interrupts.iflags & IRQ_DMA0, 0); runtime.advance_cycles(1); assert_ne!(runtime.interrupts.iflags & IRQ_DMA0, 0); assert_eq!(runtime.scheduler.now(), 50); }
     #[test] fn timer_overflow_and_cascade_share_the_same_event_boundary() { let mut runtime = Runtime::new(); runtime.interrupts.ie = IRQ_TIMER0 | IRQ_TIMER1; runtime.timers[0].write_reload(u16::MAX); runtime.timers[0].write_control(CONTROL_ENABLE | CONTROL_IRQ); runtime.timers[1].write_reload(u16::MAX); runtime.timers[1].write_control(CONTROL_ENABLE | CONTROL_CASCADE | CONTROL_IRQ); runtime.advance_cycles(2); assert_ne!(runtime.interrupts.iflags & IRQ_TIMER0, 0); assert_ne!(runtime.interrupts.iflags & IRQ_TIMER1, 0); }
     #[test] fn bitmap_ppu_renders_at_the_first_hblank_boundary() { let mut runtime = Runtime::new(); runtime.dispcnt = 3 | (1 << 10); runtime.vram[0] = 0x1f; runtime.vram[1] = 0x00; runtime.advance_cycles(HBLANK_START_CYCLES as u32); assert_eq!(runtime.ppu.framebuffer[0], 0xffff_0000); }
     #[test] fn obj_renders_at_the_hblank_boundary_after_background() { let mut runtime = Runtime::new(); runtime.dispcnt = (1 << 8) | (1 << 12); runtime.vram[0x10000..0x10004].fill(0x11); runtime.palette[2] = 0x1f; runtime.oam[4] = 1; runtime.advance_cycles(HBLANK_START_CYCLES as u32); assert_eq!(runtime.ppu.framebuffer[0], 0xffff_0000); }
+    #[test] fn serial_transfer_completes_and_requests_irq_on_central_clock() { let mut runtime = Runtime::new(); runtime.interrupts.ie = IRQ_SERIAL; runtime.write16(crate::mmio::SIOCNT, crate::sio::SIOCNT_IRQ); runtime.write8(crate::mmio::SIODATA8, 0x66); runtime.advance_cycles(crate::sio::TRANSFER_8_CYCLES as u32); assert_eq!(runtime.read8(crate::mmio::SIODATA8), 0x66); assert_ne!(runtime.interrupts.iflags & IRQ_SERIAL, 0); }
 }
